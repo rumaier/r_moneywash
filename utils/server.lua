a3de7ac07f540b9eed0a38f903a3bd76c16d007e
rumaier <148654629+rumaier@users.noreply.github.com>
2025-09-08 14:22:21 -0500

    refactor: remove database creation logic from server initialization

--- utils/server.lua
+++ utils/server.lua
@@ -4,20 +4,6 @@ local resource = GetCurrentResourceName()
 local version = GetResourceMetadata(resource, 'version', 0)
 local bridgeStarted = GetResourceState('r_bridge') == 'started'
 
-DatabaseBuilt = false
-
-local function buildDatabase()
-    local built = MySQL.query.await('SHOW TABLES LIKE "' .. resource .. '"')
-    if #built > 0 then
-        DatabaseBuilt = true
-        return
-    end
-    built = MySQL.query.await('CREATE TABLE `' .. resource .. '` ( `unit` tinyint(4) NOT NULL, PRIMARY KEY (`unit`) )')
-    if not built then print('[^8ERROR^0] - Failed to create database table for ' .. resource) end
-    print('[^2SUCCESS^0] - Database table for ' .. resource .. ' created.')
-    DatabaseBuilt = true
-end
-
 local function checkResourceVersion()
     if not Cfg.Server.VersionCheck then return end
     Core.VersionCheck(resource)
@@ -36,7 +22,6 @@ AddEventHandler('onResourceStart', function(resourceName)
     if Cfg.Debug then print(_L('debug_enabled')) end
     print('------------------------------')
     checkResourceVersion()
-    -- buildDatabase()
 end)
 
 function SendWebhook(src, event, fields)
